3967caec1a8d40b8dc9f775fe17e2d6eafc254bc
ScottieFinch <scottiefinch24@gmail.com>
2016-02-09 15:16:05 -0600

    google fonts plugin

--- wordpress/wp-content/themes/shiplify_theme/header.php
+++ wordpress/wp-content/themes/shiplify_theme/header.php
@@ -4,7 +4,15 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
-<div id="wrapper">
+<div id="container">
 <div id="header">
-<h1>HEADER</h1>
+	<ul class="menu">
+		<li class="logo-text"><img src="wp-content/themes/shiplify_theme/images/shiplify-logo.png" /><span>Shiplify<span></li>
+		<li>Carriers</li>
+		<li>Pricing</li>
+		<li>Blog</li>
+		<li>More</li>
+		<li class="login">Log in</li>
+		<li class="sign-up">Sign Up</li>
+	</ul>
 </div>
