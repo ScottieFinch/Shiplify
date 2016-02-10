2abe5e218bc7c97e9c5f2e5019c734307a1e857d
ScottieFinch <scottiefinch24@gmail.com>
2016-02-10 12:34:00 -0600

    added more header stylings and search bar

--- wordpress/wp-content/themes/shiplify_theme/header.php
+++ wordpress/wp-content/themes/shiplify_theme/header.php
@@ -6,13 +6,33 @@
 <body>
 <div id="container">
 <div id="header">
-	<ul class="menu">
-		<li class="logo-text"><img src="wp-content/themes/shiplify_theme/images/shiplify-logo.png" /><span>Shiplify<span></li>
-		<li>Carriers</li>
-		<li>Pricing</li>
-		<li>Blog</li>
-		<li>More</li>
-		<li class="login">Log in</li>
-		<li class="sign-up">Sign Up</li>
-	</ul>
+	<div class="top-menu">
+		<ul class="menu">
+			<li class="logo-text"><img src="wp-content/themes/shiplify_theme/images/shiplify-logo.png" /><span>Shiplify<span></li>
+			<li>Carriers</li>
+			<li>Pricing</li>
+			<li>Blog</li>
+			<li>More</li>
+			<li class="login">Log in</li>
+			<li class="sign-up">Sign Up</li>
+		</ul>
+	</div>
+	<div class="header-title">
+		<h1>Simplify your shipping.</h1>
+		<h2>Search lanes, find carriers, and request quotes</h2>
+	</div>
+	<div class="name-email-box">
+		<div class="centered-background">
+			<div class="a-b-img">
+				<img src="wp-content/themes/shiplify_theme/images/a-to-b.png" />
+			</div>
+			<div class="first-email-input">
+				<input class="name-input" type="text" name="fname" placeholder="First Name">
+				<input class="email-input" type="text" name="email" placeholder="Email">
+			</div>
+			<div class="search-button">
+				<button><img src="wp-content/themes/shiplify_theme/images/search-button.png" /></button>
+			</div>
+		</div>
+	</div>
 </div>
