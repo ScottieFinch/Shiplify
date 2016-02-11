8c24d0886235cfae2de15d1b17f6e1e9fbb887e7
ScottieFinch <scottiefinch24@gmail.com>
2016-02-11 13:08:48 -0600

    added responsive css for page

--- wordpress/wp-content/themes/shiplify_theme/footer.php
+++ wordpress/wp-content/themes/shiplify_theme/footer.php
@@ -1,5 +1,29 @@
 <div id="footer">
-<h1>FOOTER</h1>
+	<div class="footer-container">
+		<div class="three-column-container">
+			<div class="three-column-paragraph">
+				<img src="wp-content/themes/shiplify_theme/images/users-buildings.png" />
+				<h2>Neutral Platforms</h2>
+				<p>Shiplify is a seamless, open platform for shippers and carriers to connect directly without worrying about marked up rates.</p>
+			</div>
+			<div class="three-column-paragraph">
+				<img src="wp-content/themes/shiplify_theme/images/price-check.png" />
+				<h2>Simple pricing</h2>
+				<p>Shiplify is a seamless, open platform for shippers and carriers to connect directly without worrying about marked up rates.</p>
+			</div>
+			<div class="three-column-paragraph">
+				<img src="wp-content/themes/shiplify_theme/images/mobile-desktop-tablet.png" />
+				<h2>Works everywhere</h2>
+				<p>Shiplify is a seamless, open platform for shippers and carriers to connect directly without worrying about marked up rates.</p>
+			</div>
+		</div>
+		<div class="sign-up-button">
+			<button>Sign up Free</button>
+		</div>
+	</div>
+	<div class="logo-copyright">
+	
+	</div>
 </div>
 </div>
 </body>
--- wordpress/wp-content/themes/shiplify_theme/index.php
+++ wordpress/wp-content/themes/shiplify_theme/index.php
@@ -1,13 +1,30 @@
 <?php get_header(); ?>
 <div id="main">
 <div id="content">
-<h1>Main Area</h1>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<h1><?php the_title(); ?></h1>
-<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-<p><?php the_content(__('(more...)')); ?></p>
-<hr> <?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-<div id="delimiter">
-</div>
+	<div class="body-paragraph">
+		<img src="wp-content/themes/shiplify_theme/images/maginfying-glass.png" />
+		<h2>Search</h2>
+		<p>This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, 
+		typeset text. Only for show. He who searches for meaning here will be sorely disappointed.  These words are here to provide the 
+		reader with a basic impression of how actual text will appear in final presentation.</p>
+		<a href="#">Learn more</a>
+	</div>
+	<div class="body-paragraph">
+		<img src="wp-content/themes/shiplify_theme/images/flag.png" />
+		<h2>Quotes</h2>
+		<p>This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, 
+		typeset text. Only for show. He who searches for meaning here will be sorely disappointed.  These words are here to provide the 
+		reader with a basic impression of how actual text will appear in final presentation.</p>
+		<a href="#">Learn more</a>
+	</div>
+	<div class="body-paragraph">
+		<img src="wp-content/themes/shiplify_theme/images/map.png" />
+		<h2>Maps</h2>
+		<p>This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, 
+		typeset text. Only for show. He who searches for meaning here will be sorely disappointed.  These words are here to provide the 
+		reader with a basic impression of how actual text will appear in final presentation.</p>
+		<a href="#">Learn more</a>
+	</div>
 <?php get_footer(); ?>
+
+ 
